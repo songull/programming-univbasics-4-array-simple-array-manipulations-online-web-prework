1c022f8fa3894532fa847375c75d2793a6a8bd56
Kyler Barber <kylerbarber820@yahoo.com>
2019-06-09 04:07:02 +0000

    Automatically backed up by Learn

--- lib/intro_to_simple_array_manipulations.rb
+++ lib/intro_to_simple_array_manipulations.rb
@@ -1,4 +1,2 @@
-updated_array= using_push(colors_in_the_rainbow, next_color)
-  solo_bolo="food"
-  water.push(solo_bolo)
-end
+artists = ["Frida Kahlo", "Pablo Picasso", "Cai Guo-Qiang"]
+artists.pop
